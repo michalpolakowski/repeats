76ef66fba31d3df3662aa1eb8f1263e41c300ea1
Michał Polakowski <michal.polakowski@profil-software.com>
2019-11-30 20:16:46 +0100

    V0: second similar function

--- stockyard/utils/stockyard_files.py
+++ stockyard/utils/stockyard_files.py
@@ -3,6 +3,7 @@ from itertools import zip_longest
 
 import datetime
 from django.conf import settings
+from django.db.models import Count
 
 from stockyard.models import Repeat
 
@@ -79,5 +80,87 @@ def generate_stockyard_repeats_files():
 
     return {
         'files': generated_files,
+        'samples': len(stockyard_recall_repeat_samples)
+    }
+
+
+def generate_stockyard_split_repeats_files():
+    queryset_types = {
+        1: 'multiple',
+        2: 'methdl',
+        3: 'dilution',
+        4: 'other'
+    }
+    stockyard_recall_repeat_samples = Repeat.objects.exclude(comment__icontains="client requested") \
+        .exclude(batch_template__icontains="Pain_MA") \
+        .exclude(batch_template__icontains="ETS_MA") \
+        .exclude(batch_template__icontains="Barbs_MA") \
+        .filter(disabled=False, created__date=datetime.date.today(), laboratory__name=settings.PTOX_MAIN)
+
+    list_of_querysets = []
+
+    dupes = stockyard_recall_repeat_samples.values('accession').annotate(Count('id')).order_by().filter(id__count__gt=1)
+    multiple = stockyard_recall_repeat_samples.filter(accession__in=[sample['accession'] for sample in dupes])
+    multiple_ids = multiple.values('id')
+    list_of_querysets.append(multiple)
+
+    methdl_repeats = stockyard_recall_repeat_samples.filter(batch_template='METH_DL')
+    list_of_querysets.append(methdl_repeats)
+    meth_dl_ids = methdl_repeats.values('id')
+
+    dilution_repeats = stockyard_recall_repeat_samples.filter(comment__startswith='Interference')
+    list_of_querysets.append(dilution_repeats)
+    dilution_ids = dilution_repeats.values('id')
+
+    stockyard_recall_repeat_samples = stockyard_recall_repeat_samples \
+        .exclude(id__in=multiple_ids) \
+        .exclude(id__in=meth_dl_ids) \
+        .exclude(id__in=dilution_ids)
+    list_of_querysets.append(stockyard_recall_repeat_samples)
+
+    i = 1
+    all_generated_files = []
+    for queryset in list_of_querysets:
+        j = 1
+        generated_files_of_type = []
+        queryset_grouped = grouper(queryset, settings.STOCKYARD_GROUP_COUNT)
+        files_path = settings.LOCAL_STOCKYARD_SPLIT_REPEAT
+        check_path(files_path)
+        file_name = generate_file_name()
+
+        for group in queryset_grouped:
+            generated_file_name = file_name + f'{queryset_types[i]}-' + str(j) + '.txt'
+            stockyard_file = os.path.join(files_path, generated_file_name)
+            row = []
+
+            for repeat in group:
+                if repeat:
+                    row.append(repeat.accession)
+
+            row_to_write = ''
+            for element in row:
+                if row_to_write:
+                    row_to_write += ',' + element
+                else:
+                    row_to_write = element
+
+            row_to_write += '\n'
+
+            stockyard_open = open(stockyard_file, 'w')
+            stockyard_open.write(row_to_write)
+            stockyard_open.close()
+
+            generated_files_of_type.append(generated_file_name)
+            all_generated_files.append(generated_file_name)
+
+            j += 1
+
+            for file in generated_files_of_type:
+                os.remove('{}/{}'.format(settings.LOCAL_STOCKYARD_SPLIT_REPEAT, file))
+
+        i += 1
+
+    return {
+        'files': all_generated_files,
         'samples': len(stockyard_recall_repeat_samples)
     }
